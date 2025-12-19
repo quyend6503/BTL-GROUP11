c0516ba8cff84dc2589ddade26b420d4250aec87
Dang Quyen <quyend6503@gmail.com>
2025-12-19 23:52:45 +0700

    update trangchu

---
+++ datatintuc.js
@@ -0,0 +1,175 @@
+const newsData = [
+  {
+    id: 1,
+    title: 'Gánh Quán ra mắt món Việt mới – Tôm rang trứng muối',
+    date: '10/12/2025',
+    image: 'img/món ăn/tom-rang-trung3.jpg',
+    content: `
+      <p>Nhằm mang đến trải nghiệm ẩm thực mới mẻ hơn cho thực khách, Gánh Quán chính thức ra mắt món Tôm rang trứng muối – một món ăn kết hợp hài hòa giữa vị mặn béo của trứng muối và độ ngọt tự nhiên của tôm tươi.</p>
+      <img src="img/img tin tức/b1t1.jpg">
+      <p>Những con tôm được lựa chọn kỹ càng, chắc thịt, sau khi sơ chế sẽ được rang cùng sốt trứng muối sánh mịn, vàng óng. Khi thưởng thức, thực khách sẽ cảm nhận rõ lớp sốt đậm đà bám đều từng thớ thịt tôm, vừa béo vừa thơm nhưng không gây ngấy.
+Món ăn này rất phù hợp dùng cùng cơm trắng nóng hổi hoặc làm món ăn kèm trong các bữa tiệc gia đình, họp mặt bạn bè. Đây hứa hẹn sẽ là một trong những món “gây nghiện” mới tại Gánh Quán.</p>
+    `
+  },
+  {
+    id: 2,
+    title: 'Khám phá không gian Gánh Quán',
+    date: '10/12/2025',
+    image: 'img/img tin tức/b2t4.jpg',
+    content: `
+      <p>Không chỉ chú trọng đến chất lượng món ăn, Gánh Quán còn đặc biệt đầu tư vào không gian để mang đến cảm giác gần gũi, ấm áp cho thực khách. Lấy cảm hứng từ làng quê Việt Nam, từng góc nhỏ trong quán đều toát lên nét mộc mạc, giản dị. Gánh quán bao gồm nhiều khu vực. <br>
+      <b>Khu vực khuôn viên</b></p>
+      <img src="img/img tin tức/b2t2.jpg">
+      <img src="img/img tin tức/b2t1.jpg">      
+      <img src="img/img tin tức/b2t3.jpg">
+      
+
+      <p><b>Khu vực chỗ ngồi</b></p>
+      <img src="img/img tin tức/b2t5.jpg">
+      <img src="img/img tin tức/b2t6.jpg">
+      <img src="img/img tin tức/b2t7.jpg">
+      <img src="img/img tin tức/b2t8.jpg">
+
+      <p><b>Khu vực tổ chức sự kiện</b></p>
+      <img src="img/img tin tức/b2t9.jpg">
+      <img src="img/img tin tức/b2t10.jpg">
+      <img src="img/img tin tức/b2t11.jpg">
+
+      <p>Đến với Gánh Quán, thực khách không chỉ đến để ăn ngon mà còn để tìm lại một không gian yên bình giữa nhịp sống phố thị ồn ào.</p>
+    `
+  },
+  {
+    id: 3,
+    title: 'Top 5 món ăn được yêu thích tại Gánh Quán',
+    date: '10/12/2025',
+    image: 'img/img tin tức/b4.jpg',
+    content: `
+      <p>Sau thời gian phục vụ, Gánh Quán đã tổng hợp Top 5 món ăn được thực khách yêu thích nhất, bao gồm: <br>
+      <b>Thịt quay giòn bì</b></p>
+      <img src="iimg/mg tin tức/b3t1.jpg">
+      <p><b> Gà nướng sả ớt</b></p>
+      <img src="img/img tin tức/b3t2.jpg">
+      <p><b>Cua biển sốt me</b></p>
+      <img src="img/img tin tức/b3t3.jpg">
+      <p><b>Cải thìa xào nấm đông cô</b></p>
+      <img src="img/img tin tức/b3t4.jpg">
+      <p><b>Cá diêu hồng nướng muối ớt</b></p>
+      <img src="img/img tin tức/b3t5.jpg">
+      <p>Mỗi món ăn đều được nấu theo công thức riêng, giữ trọn hương vị truyền thống, phù hợp với nhiều lứa tuổi. Đặc biệt, các món được chế biến từ nguyên liệu tươi mới mỗi ngày nên luôn đảm bảo độ ngon và an toàn.
+Nếu bạn là khách mới, đây chính là những món “nhất định phải thử” khi ghé Gánh Quán.</p>
+
+
+
+    `
+  },
+  {
+    id: 4,
+    title: 'Hậu trường đằng sau lớp bì giòn rụm của món thịt quay giòn bì',
+    date: '10/12/2025',
+    image: 'img/img tin tức/b3.jpg',
+    content: `
+      <p>Để tạo nên món thịt quay giòn bì trứ danh, đội ngũ bếp tại Gánh Quán phải trải qua nhiều công đoạn tỉ mỉ. Thịt heo được chọn từ phần ngon nhất, sơ chế sạch sẽ, ướp gia vị theo công thức riêng.</p>
+      <img src="img/img tin tức/b4t1.jpg">
+      <p>Sau khi ướp, thịt được hong khô đúng thời gian để phần da đạt độ khô lý tưởng trước khi quay. Khi quay, nhiệt độ phải luôn được kiểm soát chuẩn xác để lớp da phồng giòn mà bên trong vẫn mềm ngọt.</p>
+      <img src="img/img tin tức/b4t2.jpg">
+      <p>Chính sự cầu kỳ trong từng bước đã tạo nên hương vị đặc trưng khó quên cho món thịt quay giòn bì tại Gánh Quán.</p>
+      <img src="img/img tin tức/b4t3.jpg">
+     
+
+
+    `
+  },
+  {
+    id: 5,
+    title: 'Gánh Quán tuyển dụng',
+    date: '17/12/2025',
+    image: 'img/img tin tức/b7t6.jpg',
+    content: `
+      <p><b>Nhân viên kỹ thuật (Toàn thời gian)</b><br>
+      <b>Mô tả công việc</b> <br>
+        – Sửa chữa các máy móc, thiết bị điện nước trong nhà hàng<br>
+      – Bảo trì bảo dưỡng máy móc<br>
+      – Sửa chữa theo order của các bộ phận<br>
+      – Lắp đặt thiết bị mới<br>
+      – Trực tiệc, âm thanh ánh sáng…<br>
+      <b>Yêu cầu</b><br>
+      – Tốt nghiệp trung cấp, cao đẳng, đại học các trường liên quan đến điện, kỹ thuật <br>
+      – Nhanh nhẹn, phản hồi và xử lý thông tin tốt<br>
+      <b>Phúc lợi</b><br>
+      – Mức lương: 9.000.000 – 15.000.000<br>
+      – Hỗ trợ ăn ca<br>
+      – Team building, nghỉ mát, chế độ BHXH…<br>
+      – Một tháng nghỉ 4 ngày, nghỉ bù ngày lễ, sinh nhật<br></p>
+      <img src="img/img tin tức/b7.jpg">
+      <p><b>Nhân viên phục vụ</b><br>
+      <b>Mô tả công việc</b><br>
+      – Đầu ca setup công cụ dụng cụ, vệ sinh khu vực theo sự phân công của ca trưởng.<br>
+      – Trong ca phục vụ khách, dọn đồ, bưng bê đồ uống.<br>
+      – Theo dõi số lượng khách, order đồ uống, thanh toán tiền.<br>
+      – Cuối ca dọn dẹp đảm bảo vệ sinh cho ca sau làm việc…<br>
+      <b>Ca làm việc</b>: 10h – 14h30 & 17h – 22h<br>
+      <b>Yêu cầu</b><br>
+      – Trên 18 tuổi<br>
+      – Chăm chỉ, nhanh nhẹn<br>
+      – Tính tình dễ thương, trách nghiệm trong công việc<br>
+      – Không yêu cầu kinh nghiệm<br>
+      <b>Phúc lợi</b><br>
+      – Mức lương: 7.500.000 – 8.000.000<br>
+      – Hỗ trợ ăn ca<br>
+      – Team building, nghỉ mát, chế độ BHXH…<br>
+      – Típ, doanh thu, thưởng chuyên cần<br>
+      – Một tháng nghỉ 4 ngày, nghỉ bù ngày lễ, sinh nhật<br></p>
+      <p><br><b>Hồ sơ ứng tuyển gửi về email</b>: Quyend6503@gmail.com(Ms. Quyen)</p>
+
+    `
+  },
+  {
+    id: 6,
+    title: 'Bếp trưởng – Người giữ lửa cho hương vị Gánh Quán',
+    date: '07/12/2025',
+    image: 'img/img tin tức/b6t1.jpg',
+    content: `
+      <p>Đứng sau gian bếp của Gánh Quán là hình ảnh bếp trưởng Phạm Tuấn Hải– người được xem là “linh hồn” của quán. Với nhiều năm kinh nghiệm trong nghề, bếp trưởng không chỉ nấu ăn ngon mà còn là người giữ trọn tinh thần ẩm thực Việt truyền thống.</p>
+      <img src="img/img tin tức/b6t2.jpg">
+      <p>Mỗi món ăn đều được bếp trưởng kiểm tra kỹ lưỡng từ hương vị đến cách trình bày trước khi phục vụ thực khách. Không cầu kỳ kiểu cách, các món ăn tại Gánh Quán luôn giữ sự mộc mạc nhưng đậm đà và vừa miệng. <br>
+      Bếp trưởng cũng chính là người truyền cảm hứng, hướng dẫn đội ngũ phụ bếp từng ngày để giữ vững chất lượng ổn định cho từng bữa ăn.</p>
+    `
+  },
+  {
+    id: 7,
+    title: 'Gánh Quán – Điểm hẹn lý tưởng cho những bữa tiệc gia đình ấm cúng',
+    date: '02/12/2025',
+    image: 'img/img tin tức/b7.jpg',
+    content: `
+      <p>Giữa nhịp sống bận rộn, những bữa cơm sum vầy cùng gia đình luôn là khoảnh khắc đáng trân quý. Hiểu được điều đó, Gánh Quán không chỉ mang đến những món ăn đậm đà hương vị Việt mà còn là không gian lý tưởng để mọi người quây quần, chia sẻ và gắn kết.</p>
+      <p><b>Khu vực nhà cổ</b></p>
+      <img src="img/img tin tức/b7t1.jpg">
+      <p><b>Khu vực VIP 1</b></p>
+      <img src="img/img tin tức/b7t2.jpg">
+      <p><b>Khu vực VIP 2</b></p>
+      <img src="img/img tin tức/b7t3.jpg">
+      <p><b>Khu vực hoàng gia</b></p>
+      <img src="img/img tin tức/b7t4.jpg">
+      <p>Không gian quán được thiết kế theo phong cách tối giản, gần gũi với bàn ghế gỗ hoặc ghế nệm, không gian kết hợp giữa hiện đại nhưng vẫn mang nét truyền thống tạo cảm giác thân thuộc như đang dùng bữa tại chính ngôi nhà của mình. Dù là bữa tiệc sinh nhật, liên hoan gia đình hay đơn giản là một buổi tụ họp cuối tuần, Gánh Quán luôn tạo được sự thoải mái và ấm áp cho thực khách.
+      Thực đơn tại quán vô cùng phong phú Mỗi món ăn đều được chế biến từ nguyên liệu tươi ngon, nêm nếm vừa miệng, phù hợp với mọi lứa tuổi.
+      Không chỉ chú trọng chất lượng món ăn, đội ngũ nhân viên Gánh Quán luôn phục vụ với thái độ tận tình, chu đáo, mang lại cho khách hàng cảm giác dễ chịu trong suốt bữa ăn.</p>
+      <p>Gánh Quán không chỉ là nơi thưởng thức ẩm thực, mà còn là nơi lưu giữ những khoảnh khắc sum vầy trọn vẹn bên gia đình và người thân.</p>
+    `
+  },
+  {
+    id: 8,
+    title: 'Vì sao thực khách luôn quay lại Gánh Quán sau lần đầu ghé thăm?',
+    date: '01/12/2025',
+    image: 'img/img tin tức/b8.jpg',
+    content: `
+      <p>Nhiều thực khách chia sẻ rằng, Gánh Quán không chỉ khiến họ hài lòng bởi món ăn ngon, mà còn bởi sự thân thiện từ phong cách phục vụ cho đến không gian quán.</p>
+      <img src="img/img tin tức/b8t1.jpg">
+      <p>Món ăn giữ được chất lượng ổn định, không gian ấm áp, giá cả hợp lý, phục vụ nhanh chóng – tất cả tạo nên một trải nghiệm trọn vẹn. Dù là bữa cơm gia đình, buổi họp mặt bạn bè hay tiệc liên hoan nhỏ, Gánh Quán đều mang đến cảm giác gần gũi, thoải mái.</p>
+      <img src="img/img tin tức/b8t2.jpg">
+      <img src="img/img tin tức/b8t3.jpg">
+      <p>Chính những điều đó đã khiến nhiều thực khách quay lại Gánh Quán không chỉ một lần.</p>
+
+
+    `
+  }
+];
